3b3c005b4ff6efa84a6dcc877f55d2e62d43f645
TMGBNoodle <tm.grovesbenedict@gmail.com>
2025-02-27 21:14:35 -0600

    Movement Functional

--- CarrotInjector/Assets/Scripts/PlayerMovement.cs
+++ CarrotInjector/Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -16,6 +17,16 @@ public class PlayerMovement : MonoBehaviour
     public Animator animator;
     
     public float jumpPower;
+
+    public float maxSpeed = 5;
+
+    public float debounce = 0.1f;
+
+    public float accel = 10f;
+    
+    public float decel = 15;
+
+    float jumptime = 0;
 
     Rigidbody2D body;
     public bool isGrounded = true;
@@ -36,11 +47,11 @@ public class PlayerMovement : MonoBehaviour
     }
 
     void doJump(int jumpCount) {
+        animator.SetInteger("Jumps", jumpCount+1);
+        jumptime = Time.time;
         print("Jumping");
         isGrounded = false;
         body.AddForceY(jumpPower);
-        animator.SetInteger("Jumps", jumpCount+1);
-        
     }
 
     // Update is called once per frame
@@ -51,10 +62,11 @@ public class PlayerMovement : MonoBehaviour
         float jumpInfo = jump.ReadValue<float>();
         int jumpCount = animator.GetInteger("Jumps");
         bool tryJump = moveInfo.y > 0 || jumpInfo > 0;
-        print(transform.position);
-        Collider2D[] collisions = Physics2D.OverlapCircleAll(transform.position, 0.05f);
+        float timeSinceJump = Time.time - jumptime;
+        bool canJump = timeSinceJump >= debounce;
+        Collider2D[] collisions = Physics2D.OverlapCircleAll(transform.position, 0.1f);
         foreach (Collider2D collision in collisions) {
-            if (collision.gameObject.tag == "Floor" && body.linearVelocityY <= 0) {
+            if (collision.gameObject.tag == "Floor" && body.linearVelocityY <= 0 && canJump) {
                 if(isGrounded == false) {
                     body.linearVelocityY = 0;
                     isGrounded = true;
@@ -63,11 +75,20 @@ public class PlayerMovement : MonoBehaviour
                 }
             }
         }
-        if (tryJump) {
+        if (tryJump && canJump) {
             if (jumpCount < maxJumps || isGrounded) {
                 doJump(jumpCount);
             }
         }
-        
+        float goalSpeed = xMove * maxSpeed;
+        print(goalSpeed);
+        if (Math.Abs(goalSpeed) > Math.Abs(body.linearVelocityX) || Math.Sign(goalSpeed) != Math.Sign(body.linearVelocityX)) {
+            body.AddForceX(accel * xMove);
+        } else if (goalSpeed == 0 && Math.Abs(body.linearVelocityX) > 0) {
+            body.AddForceX(decel * -1 * Math.Sign(body.linearVelocityX));
+        } 
+        if(Math.Abs(goalSpeed - body.linearVelocityX) < 0.1f) {
+            body.linearVelocityX = goalSpeed;
+        }
     }
 }
